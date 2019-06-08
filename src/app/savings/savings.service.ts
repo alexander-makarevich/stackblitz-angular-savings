027a73a1b6b449165d0756acfbaaf931e412d187
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-08 17:48:45 +0700

    edit calculate

--- src/app/savings/savings.service.ts
+++ src/app/savings/savings.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
 
+
+interface Row {
+  year: number;
+  month: number;
+  economy: number;
+  interest: number;
+  sumPerMonth: number;
+  sumInterest: number;
+}
+
 @Injectable()
 export class SavingsService {
+  readonly income = 30000;
+  readonly interestRate = 0.02;
+  readonly years = 30;
 
   constructor() { }
 
   calculate(): string {
+    let account = 0;
+    const sumInterest = 0;
+    for (let m = 0; m < this.years * 12; ++m) {
+      const interest = account * this.interestRate;
+      account += interest;
+      const monthsSaved = 0;
+    }
     return 'SavingsService->calculate()';
   }
 
