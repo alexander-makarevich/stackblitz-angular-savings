cc52ea0f1d486f9bbcf8e1774d10ace5ad7117d3
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-08 16:24:19 +0700

    add savings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+import { SavingsComponent } from './savings/savings.component';
 
 @NgModule({
   imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MaterialModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SavingsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
