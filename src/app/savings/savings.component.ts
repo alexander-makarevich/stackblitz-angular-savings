026ccb06454116e46126246ae321ee71b0e85e86
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-08 16:55:56 +0700

    add service

--- src/app/savings/savings.component.ts
+++ src/app/savings/savings.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SavingsService } from './savings.service';
+
 @Component({
   selector: 'app-savings',
   templateUrl: './savings.component.html',
-  styleUrls: ['./savings.component.css']
+  styleUrls: ['./savings.component.css'],
+  providers: [SavingsService]
 })
 export class SavingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SavingsService) { }
 
   ngOnInit() {
+    console.log(this.service.calculate());
   }
 
 }
