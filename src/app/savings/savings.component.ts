f48248430750fdd8d3c1e2232f36f283b68df33d
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-09 10:19:08 +0700

    define decimal delimiter

--- src/app/savings/savings.component.ts
+++ src/app/savings/savings.component.ts
@@ -13,13 +13,13 @@ import { SavingsService, Row } from './savings.service';
 export class SavingsComponent implements OnInit {
   income = new FormControl(30000, [
     Validators.required,
-    Validators.pattern("^[0-9]*$"),
+    Validators.pattern("^[0-9.]*$"),
     Validators.min(0),
     Validators.max(100000)
   ]);
   interestRate = new FormControl(0.02, [
     Validators.required,
-    Validators.pattern("^[0-9]*$"),
+    Validators.pattern('^[0-9.]*$'),
     Validators.min(0),
     Validators.max(100)
   ]);
