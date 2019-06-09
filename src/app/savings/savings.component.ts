983f9975631a23779e8de3cf9756944abd09bd89
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-09 09:58:18 +0700

    add required and number

--- src/app/savings/savings.component.ts
+++ src/app/savings/savings.component.ts
@@ -11,8 +11,18 @@ import { SavingsService, Row } from './savings.service';
   providers: [SavingsService]
 })
 export class SavingsComponent implements OnInit {
-  income = new FormControl(30000, [Validators.min(0), Validators.max(100000)]);
-  interestRate = new FormControl(0.02, [Validators.min(0), Validators.max(100)]);
+  income = new FormControl(30000, [
+    Validators.required,
+    Validators.pattern("^[0-9]*$"),
+    Validators.min(0),
+    Validators.max(100000)
+  ]);
+  interestRate = new FormControl(0.02, [
+    Validators.required,
+    Validators.pattern("^[0-9]*$"),
+    Validators.min(0),
+    Validators.max(100)
+  ]);
 
   rows: Row[] = [];
   displayedColumns = ['month', 'account', 'interest', 'totalInterest', 'savedMonthsCount'];
